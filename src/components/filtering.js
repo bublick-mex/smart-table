ef597ee8e1574d8fd3c2cbc3f2cd33817bff48cd
bublick-mex <lavrinovichnastia@gmail.com>
2026-07-04 21:41:35 +0300

    Табоица с реализованной фильтрацией, поиском и сортировкой, а также перелистыванием страниц

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -30,6 +30,7 @@ export function initFiltering(elements, indexes) {
                 state[field] = '';
             }
         }
+        const { totalFrom, totalTo, ...filterState } = state;
         if (totalFrom || totalTo) {
             filterState.total = [
                 totalFrom ? parseFloat(totalFrom) : null,
@@ -37,6 +38,6 @@ export function initFiltering(elements, indexes) {
             ];
         }
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data.filter(row => compare(row, state));
+        return data.filter(row => compare(row, filterState));
     }
 }
