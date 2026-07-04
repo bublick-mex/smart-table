7775039740027786ae8c50529360841045a8296a
bublick-mex <lavrinovichnastia@gmail.com>
2026-07-04 21:25:46 +0300

    Табоица с реализованной фильтрацией, поиском и сортировкой, а также перелистыванием страниц

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -4,11 +4,33 @@ import {createComparison, defaultRules} from "../lib/compare.js";
 
 export function initFiltering(elements, indexes) {
     // @todo: #4.1 — заполнить выпадающие списки опциями
-
+    Object.keys(indexes)                                    // Получаем ключи из объекта
+      .forEach((elementName) => {                        // Перебираем по именам
+        elements[elementName].append(                    // в каждый элемент добавляем опции
+            ...Object.values(indexes[elementName])        // формируем массив имён, значений опций
+                      .map(name => {                        // используйте name как значение и текстовое содержимое
+                        const option = document.createElement('option');
+                        option.value = name;
+                        option.textContent = name;
+                        return option;
+                      })
+        )
+     })
     return (data, state, action) => {
         // @todo: #4.2 — обработать очистку поля
-
+        if (action && action.name === 'clear') {
+            const parent = action.parentElement;
+            const input = parent.querySelector('input');
+            if (input) {
+                input.value = '';
+            }
+            const field = action.dataset.field;
+            if (field && state) {
+                state[field] = '';
+            }
+        }
         // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+        const compare = createComparison(defaultRules);
+        return data.filter(row => compare(row, state));
     }
 }
